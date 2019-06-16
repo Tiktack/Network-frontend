9209d6be09edc658ae2a463f6f5cc9c32689e851
Aleh_khantsevich <aleh_khantsevich@epam.com>
2019-06-16 16:36:57 +0300

    signalR middleware was deleted

--- src/redux/middlewares/index.js
+++ src/redux/middlewares/index.js
@@ -1,7 +1,6 @@
 import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import signalRMiddleware from './signalR.middleware';
 
 
-export default applyMiddleware(thunk, logger, signalRMiddleware);
+export default applyMiddleware(thunk, logger);
