212c0b4530cbcd169117fcd534dd3191d38a3d62
Aleh_khantsevich <aleh_khantsevich@epam.com>
2019-07-18 17:39:35 +0300

    1

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,4 @@
 /* eslint-disable prefer-destructuring */
-export const API_URL_BASE = process.env.API_URL_BASE || 'https://localhost:4000/';
+export const API_URL_BASE = process.env.API_URL_BASE || 'https://social-network-backend/';
 export const AUTHENTICATION_ENDPOINT = 'account';
 export const USER_ENDPOINT = 'user';
